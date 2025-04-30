e7b84d2e22f834c9a5a60c2d409e4d66f424013d
zeru-program <justinebogor0609@gmail.com>
2025-04-30 22:07:52 +0700

    update: env mysql

--- backend/migration/laporan_service/knexfile.js
+++ backend/migration/laporan_service/knexfile.js
@@ -3,11 +3,11 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host: 'tokala.iixcp.rumahweb.net',
-      user: 'indy4153_simkuy',
-      password: 'simajukuyEPDB00!',
-      database: 'indy4153_simajukuy_laporan',
-      port: 2083,
+      host: 'dbpplg.smkn4bogor.sch.id',
+      user: 'pplg',
+      password: 'adminpplg2025!',
+      database: 'simajukuy_laporan',
+      port: 6093,
     },
     migrations: {
       directory: './migrations',
