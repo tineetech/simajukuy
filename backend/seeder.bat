d1d0d8899366632e818ba3ac239ad1a4ed3dfda8
zeru-program <justinebogor0609@gmail.com>
2025-05-14 12:53:11 +0700

    updated seeder.bat

--- backend/seeder.bat
+++ backend/seeder.bat
@@ -1,7 +1,7 @@
 @echo off
 @REM cd /d C:\Project\Sikuy1\backend
 
+start cmd /k "cd migration/user_service && npx knex seed:run"
 start cmd /k "cd migration/laporan_service && npx knex seed:run"
 start cmd /k "cd migration/post_service && npx knex seed:run"
-start cmd /k "cd migration/user_service && npx knex seed:run"
 start cmd /k "cd migration/notification_service && npx knex seed:run"
